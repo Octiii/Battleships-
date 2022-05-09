4b56c850cc05d9cd746cd3282ec4e9a0ef6ff711
Octiii <sirocti@gmail.com>
2022-05-09 14:35:36 +0000

    Made a grid setting code that is costomisable by the user

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+grid_size = input("How big would you like the grid to be?")
+"""selection = input("Please select a cell.")"""
+grid = []
+
+for x in range(int(grid_size)):
+    x = []
+    
+    for y in range(int(grid_size)):
+        y = []
+        x.append(y)
+    grid.append(x)
+
+
+"""
+Print staments to check on code.
+"""
+for y in range(len(grid)):
+    print(grid[y])
