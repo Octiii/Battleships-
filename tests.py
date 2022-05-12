b6bf3cf4643ad663bd868bf3b1c9cde558c90d72
Octi <sirocti@gmail.com>
2022-05-12 16:31:42 +0100

    Using the join fucntion to clean up the board!

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ board = []
 for i in range(0,5):
     board.append(["O"] *int(grid_size))
 def print_board(board):
-    for x in board
-        print x
+    for x in board:
+        print(" ". join(x))
 
 print_board(board)
