7779e3a68088614cc9dd4908d9a3baf75797ac75
Octi <sirocti@gmail.com>
2022-05-12 17:20:40 +0100

    Added random ship spawner.

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
+"""
+from random import randit
+"""
+
 grid_size = input("How big would you like the grid to be?")
 board = []
 
@@ -8,3 +12,14 @@ def print_board(board):
         print(" ". join(x))
 
 print_board(board)
+
+def random_row(board):
+    return randint(0,len(board)-1)
+
+def random_col(board):
+    return randint(0,len(board)-1)
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Column: "))
